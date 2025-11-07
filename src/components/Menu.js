067f1a1cc1e9e8623722e7709abef307a57373b4
Dev-13552 <devrathour110@gmail.com>
2025-11-07 19:48:33 +0530

    Completed3

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 
 const Menu = ({menu, categories, filter, setFilter}) => {
     const [filteredArray, setFilteredArray] = useState(menu)
-
+    console.log(filteredArray)
     useEffect(()=>{
         if(filter !== "All"){
             setFilteredArray(menu.filter((item) => filter===item.category))
@@ -10,22 +10,26 @@ const Menu = ({menu, categories, filter, setFilter}) => {
         else{
             setFilteredArray(menu)
         }
-    },[filter])
+        
+    },[filter, menu])
   return (
-    <div>
-      {
+    <div id='main'>
+      {/* {
         categories.map((category, index)=>(
             <button id = {`filter-button-${index}`} onClick={()=>{setFilter(category)}} key={index}>{category}</button>
         ))
         
-      }
+      } */}
+      <button id='filter-button-0' onClick={()=>{setFilter('All')}}>All</button>
+      <button id='filter-button-1' onClick={()=>{setFilter('Breakfast')}}>BreakFast</button>
+      <button id='filter-button-2' onClick={()=>{setFilter('Lunch')}}>Lunch</button>
+      <button id='filter-button-3' onClick={()=>{setFilter('Shakes')}}>Shakes</button>
       {
         filteredArray.map((item)=>(
             <div key={item.id}>
                 <div>{item.name}</div>
-                <div>{item.categories}</div>
+                <div>{item.category}</div>
                 <div>{item.price}</div>
-
             </div>
         ))
       }
